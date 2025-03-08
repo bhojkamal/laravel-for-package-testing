1886bcf2c237c370081f264cadf1c1571457cf91
Lotussoft Youngtech <lotussoftyoungtech@gmail.com>
2025-03-08 14:28:07 +0545

    interface subfolder bool

--- config/config.php
+++ config/config.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-  'repository_path' => 'app/Repositories',
+  'repository_path' => env('REPOSITORY_PATH', base_path('app/Repositories')),
+  'interface_subfolder' => env('REPOSITORY_INTERFACE_SUBFOLDER', true),
   'transformer_path' => 'app/Http/Transformers',
   'filter_path' => 'app/Http/Filters'
 ];
