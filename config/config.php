3fb974f89833060e5414cab4f622e78e9610b95f
Lotussoft Youngtech <lotussoftyoungtech@gmail.com>
2025-03-09 12:45:15 +0545

    minor update

--- config/config.php
+++ config/config.php
@@ -3,6 +3,6 @@
 return [
   'repository_path' => env('REPOSITORY_PATH', base_path('app/Repositories')),
   'interface_subfolder' => env('REPOSITORY_INTERFACE_SUBFOLDER', true),
-  'transformer_path' => 'app/Http/Transformers',
-  'filter_path' => 'app/Http/Filters'
+  'transformer_path' => env('TRANSFORMER_PATH', 'app/Http/Transformers'),
+  'filter_path' => env('FILTER_PATH', 'app/Http/Filters'),
 ];
